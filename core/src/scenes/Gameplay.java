ec2e1916c1a3ef713fa7db9d334f9a3e5a1f8ff2
StanArts <stanimirgevel@gmail.com>
2020-09-16 18:29:15 +0300

    Created the Main Camera and imported some of the assets

---
+++ core/src/scenes/Gameplay.java
@@ -0,0 +1,87 @@
+package scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.stangvel.dipin.GameMain;
+
+import helpers.GameInfo;
+
+public class Gameplay implements Screen {
+
+    private GameMain game;
+
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
+    Texture img;
+
+    public Gameplay (GameMain game) {
+        this.game = game;
+
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+
+        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
+
+        img = new Texture("Player/Player.png");
+    }
+
+    void update(float dt) {
+        moveCamera();
+    }
+
+    void moveCamera() {
+        mainCamera.position.y -= 1;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        update(delta);
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.getBatch().begin();
+        game.getBatch().draw(img, 0, 0);
+        game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- desktop/src/com/stangvel/dipin/desktop/DesktopLauncher.java
+++ desktop/src/com/stangvel/dipin/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.stangvel.dipin.GameMain;
 
+import helpers.GameInfo;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.setWindowedMode(GameInfo.WIDTH, GameInfo.HEIGHT);
+
 		new Lwjgl3Application(new GameMain(), config);
 	}
 }
