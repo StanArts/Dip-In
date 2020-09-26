e6e3bf9717d6a028c5beaff72fa0131cbd13f38b
StanArts <stanimirgevel@gmail.com>
2020-09-26 18:55:32 +0100

    Making the background changes its colour smoothly

--- core/src/scenes/Gameplay.java
+++ core/src/scenes/Gameplay.java
@@ -3,14 +3,22 @@ package scenes;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.ColorAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.stangvel.dipin.GameMain;
+
+import java.util.Random;
 
 import helpers.GameInfo;
 import platforms.PlatformsController;
@@ -19,6 +27,17 @@ import player.Player;
 public class Gameplay implements Screen {
 
     private GameMain game;
+
+    Color color;
+    Color endColor;
+    Color activeColor;
+    ColorAction actionStE = new ColorAction();
+    ColorAction actionEtS = new ColorAction();
+    SequenceAction sequenceAction;
+    ShapeRenderer testRenderer;
+    RepeatAction repeatAction = new RepeatAction();
+
+    private Actor actionManager = new Actor();
 
     private OrthographicCamera mainCamera;
     private Viewport gameViewport;
@@ -31,8 +50,30 @@ public class Gameplay implements Screen {
     private PlatformsController platformsController;
     private Player player;
 
+    Color[] colors = {Color.YELLOW, Color.GREEN, Color.BLUE};
+
+    Random generator = new Random();
+
     public Gameplay (GameMain game) {
         this.game = game;
+
+        testRenderer = new ShapeRenderer();
+        color = new Color(Color.RED);
+        endColor = new Color(randomColor());
+        activeColor = new Color(Color.RED);
+
+        actionStE.setColor(activeColor);
+        actionStE.setDuration(5);
+        actionStE.setEndColor(endColor);
+
+        actionEtS.setColor(activeColor);
+        actionEtS.setDuration(5);
+        actionEtS.setEndColor(color);
+
+        sequenceAction = new SequenceAction(actionStE, actionEtS);
+        repeatAction.setAction(sequenceAction);
+        repeatAction.setCount(RepeatAction.FOREVER);
+        actionManager.addAction(repeatAction);
 
         mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
         mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
@@ -77,13 +118,38 @@ public class Gameplay implements Screen {
         mainCamera.position.y -= 1f;
     }
 
+    Color randomColor() {
+
+        int randomIndex = generator.nextInt(colors.length);
+        return colors[randomIndex];
+    }
+
+//    void changeColor(float dt) {
+//
+//        Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//        colorAction.act(dt);
+//
+//    }
+
     @Override
     public void render(float delta) {
         update(delta);
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        changeColor(delta);
 
+        Gdx.gl.glClearColor(activeColor.r,activeColor.g,activeColor.b,activeColor.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        repeatAction.act(delta);
+
+
+//        testRenderer.begin(ShapeRenderer.ShapeType.Filled);
+//        testRenderer.setColor(activeColor);
+//        testRenderer.rect(100, 100, 40, 40);
+//        testRenderer.end();
+
+        actionManager.act(delta);
         game.getBatch().begin();
 
         platformsController.drawPlatforms(game.getBatch());
